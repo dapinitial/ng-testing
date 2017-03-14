37b4b398653bbddfdba5893deef71a74635fbc64
David Puerto <me@davidpuerto.com>
2017-03-14 01:21:51 -0700

    fixing the ng-repeat

--- app/view/dashboard/dashboard-controller.js
+++ app/view/dashboard/dashboard-controller.js
@@ -9,17 +9,17 @@ function DashboardController($log, $rootScope, productService) {
 
   this.products = [];
 
-   this.fetchProducts = function() {
-     productService.fetchProducts()
-     .then( products => {
-       this.products = products;
-     });
-   };
+  this.fetchProducts = function() {
+    productService.fetchProducts()
+    .then( products => {
+      this.products = products;
+    });
+  };
 
-   this.fetchProducts();
+  this.fetchProducts();
 
-   $rootScope.$on('$locationChangeSuccess', () => {
-     this.fetchProducts();
-   });
+  $rootScope.$on('$locationChangeSuccess', () => {
+    this.fetchProducts();
+  });
 
-};
+}
